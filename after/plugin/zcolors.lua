259aef983eba69fcc2149963230bba84dc35223a
johnmarco123 <johnmarcotasillo@gmail.com>
2024-09-07 16:05:49 -0400

    added oil!

--- after/plugin/zcolors.lua
+++ after/plugin/zcolors.lua
@@ -3,6 +3,12 @@ function ColorMyPencils(color)
     vim.cmd.colorscheme(color)
     vim.api.nvim_set_hl(0, 'LineNr', { fg="#00FFFF" })
     vim.api.nvim_set_hl(0, 'Comment', { fg="#FF0000" })
+    -- Set color for absolute line numbers
+    --vim.cmd('highlight LineNr guifg=#FF0000')
+
+    -- Set color for relative line numbers
+    vim.cmd('highlight LineNrAbove guifg=#888888')
+    vim.cmd('highlight LineNrBelow guifg=#888888')
 --  vim.api.nvim_set_hl(0, 'Visual', {bg="#00FF88", fg="#000000"})
 
 --   vim.api.nvim_set_hl(0, 'DiagnosticUnnecessary', {fg="#FF5F1F"})
