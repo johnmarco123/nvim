3ebf8b2fd9d9ec8133df5091b6bbafb8cef27fe9
johnmarco123 <johnmarcotasillo@gmail.com>
2024-11-20 20:52:29 -0500

    made telescope also index git ignore files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,18 +2,22 @@ return { 'nvim-telescope/telescope.nvim',
     dependencies = { {'nvim-lua/plenary.nvim'} },
     config = function()
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+        vim.keymap.set('n', '<leader>pf', function()
+            builtin.find_files({ no_ignore = true })
+        end)
+        vim.keymap.set('n', '<C-p>', function()
+            builtin.git_files ({ no_ignore = true })
+        end)
         vim.keymap.set('n', '<leader>pws', function()
             local word = vim.fn.expand("<cword>")
             builtin.grep_string({ search = word });
         end)
         vim.keymap.set('n', '<leader>pWs', function()
             local word = vim.fn.expand("<cWORD>")
-            builtin.grep_string({ search = word });
+            builtin.grep_string({ search = word, no_ignore = true});
         end)
         vim.keymap.set('n', '<leader>ps', function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") });
+            builtin.grep_string({ search = vim.fn.input("Grep > "), no_ignore = true });
         end)
         require('telescope').setup{
             defaults = {
