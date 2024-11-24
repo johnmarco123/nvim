51ae9da9f9698648e9779d2fea8ca3faa629bb5c
johnmarco123 <johnmarcotasillo@gmail.com>
2024-11-24 16:27:30 -0500

    gitsigns is amazing... lets add it

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,7 +1,21 @@
 return {
     'lewis6991/gitsigns.nvim',
     config = function()
-        require('gitsigns').setup {
+        local gitsigns = require('gitsigns')
+        vim.keymap.set('n', '<leader>hs', gitsigns.stage_hunk)
+        vim.keymap.set('n', '<leader>hr', gitsigns.reset_hunk)
+        vim.keymap.set('v', '<leader>hs', function() gitsigns.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+        vim.keymap.set('v', '<leader>hr', function() gitsigns.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+        vim.keymap.set('n', '<leader>hS', gitsigns.stage_buffer)
+        vim.keymap.set('n', '<leader>hu', gitsigns.undo_stage_hunk)
+        vim.keymap.set('n', '<leader>hR', gitsigns.reset_buffer)
+        vim.keymap.set('n', '<leader>hp', gitsigns.preview_hunk)
+        vim.keymap.set('n', '<leader>hb', function() gitsigns.blame_line{full=true} end)
+        vim.keymap.set('n', '<leader>tb', gitsigns.toggle_current_line_blame)
+        vim.keymap.set('n', '<leader>hd', gitsigns.diffthis)
+        vim.keymap.set('n', '<leader>hD', function() gitsigns.diffthis('~') end)
+        vim.keymap.set('n', '<leader>tD', gitsigns.toggle_deleted)
+        gitsigns.setup {
           signs = {
             add          = { text = '┃' },
             change       = { text = '┃' },
@@ -21,7 +35,7 @@ return {
           signs_staged_enable = true,
           signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
           numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
-          linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
+          linehl     = true, -- Toggle with `:Gitsigns toggle_linehl`
           word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
           watch_gitdir = {
             follow_files = true
@@ -32,7 +46,7 @@ return {
           current_line_blame_opts = {
             virt_text = true,
             virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
-            delay = 1000,
+            delay = 500,
             ignore_whitespace = false,
             virt_text_priority = 100,
             use_focus = true,
